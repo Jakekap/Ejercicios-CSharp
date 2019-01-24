ec32d8d229a706697798fd571dd0880d19a46ae7
Christian Andrade <46961345+ChistianA@users.noreply.github.com>
2019-01-24 16:40:03 -0500

    Add files via upload

---
+++ Desafio 02-03.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Desafio_02_03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Ingreso de datos
+            Console.WriteLine("Ingrese del valor del cateto mayor del Triangulo Rectangulo >>");
+            double z = double.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese del valor del angulo formado por la hipotenusa y el cateto menor >>");
+            double c = double.Parse(Console.ReadLine());
+
+            // Operaciones Matematicas
+            double t = Math.Round(z / Math.Sin(Math.PI * c / 180), 2);
+            double y = Math.Round(z / Math.Tan(Math.PI * c / 180), 2);
+            double a = Math.Round(90 - c, 2);
+
+
+            // Salida de datos
+            Console.WriteLine("El valor del cateto menor es >> " + y);
+            Console.WriteLine("El valor de la hipotenusa es >> " + t);
+            Console.WriteLine("El angulo formado por la hipotenusa y el cateto mayor es >> " + a);
+        }
+    }
+}
