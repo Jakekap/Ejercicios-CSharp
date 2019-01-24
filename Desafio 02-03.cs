dc7d1937883764e28030216b3820ff254df5b67a
Christian Andrade <46961345+ChistianA@users.noreply.github.com>
2019-01-24 17:24:53 -0500

    Update Desafio 02-03.cs

--- Desafio 02-03.cs
+++ Desafio 02-03.cs
@@ -13,19 +13,26 @@ namespace Desafio_02_03
             // Ingreso de datos
             Console.WriteLine("Ingrese del valor del cateto mayor del Triangulo Rectangulo >>");
             double z = double.Parse(Console.ReadLine());
-            Console.WriteLine("Ingrese del valor del angulo formado por la hipotenusa y el cateto menor >>");
+            Console.WriteLine("Ingrese del valor del angulo formado por la hipotenusa y el cateto menor\n(Debe ser mayor o igual a 45°) >>");
             double c = double.Parse(Console.ReadLine());
 
-            // Operaciones Matematicas
-            double t = Math.Round(z / Math.Sin(Math.PI * c / 180), 2);
-            double y = Math.Round(z / Math.Tan(Math.PI * c / 180), 2);
-            double a = Math.Round(90 - c, 2);
+            if(c >= 45)
+            {
+                // Operaciones Matematicas
+                double t = Math.Round(z / Math.Sin(Math.PI * c / 180), 2);
+                double y = Math.Round(z / Math.Tan(Math.PI * c / 180), 2);
+                double a = Math.Round(90 - c, 2);
 
 
-            // Salida de datos
-            Console.WriteLine("El valor del cateto menor es >> " + y);
-            Console.WriteLine("El valor de la hipotenusa es >> " + t);
-            Console.WriteLine("El angulo formado por la hipotenusa y el cateto mayor es >> " + a);
+                // Salida de datos
+                Console.WriteLine("El valor del cateto menor es >> " + y);
+                Console.WriteLine("El valor de la hipotenusa es >> " + t);
+                Console.WriteLine("El angulo formado por la hipotenusa y el cateto mayor es >> " + a);
+            }
+            else
+            {
+                Console.WriteLine("El angulo debe ser mayor o igual a 45°!");
+            }
         }
     }
 }
