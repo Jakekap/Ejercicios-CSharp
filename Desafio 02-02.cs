7a244724804f057ce3e444db4ff01d66a169c88f
Christian Andrade <46961345+ChistianA@users.noreply.github.com>
2019-01-24 17:16:45 -0500

    Update Desafio 02-02.cs

--- Desafio 02-02.cs
+++ Desafio 02-02.cs
@@ -17,16 +17,23 @@ namespace Desafio_02_02
             Console.WriteLine("Ingrese del valor del angulo formado por la hipotenusa y el cateto mayor\n(Debe ser menor o igual a 45°) >>");
             double a = double.Parse(Console.ReadLine());
 
-            // Operaciones Matematicas
-            double z = Math.Round(t * Math.Cos(Math.PI * a / 180),2);
-            double y = Math.Round(t * Math.Sin(Math.PI * a / 180),2);
-            double c = Math.Round(90 -a, 2);
+            if (a <= 45)
+            {
+                // Operaciones Matematicas
+                double z = Math.Round(t * Math.Cos(Math.PI * a / 180), 2);
+                double y = Math.Round(t * Math.Sin(Math.PI * a / 180), 2);
+                double c = Math.Round(90 - a, 2);
 
 
-            // Salida de datos
-            Console.WriteLine("El valor del cateto mayor es >> " + z);
-            Console.WriteLine("El valor del cateto menor es >> " + y);
-            Console.WriteLine("El angulo formado por la hipotenusa y el cateto menor es >> " + c);
+                // Salida de datos
+                Console.WriteLine("El valor del cateto mayor es >> " + z);
+                Console.WriteLine("El valor del cateto menor es >> " + y);
+                Console.WriteLine("El angulo formado por la hipotenusa y el cateto menor es >> " + c);
+            }
+            else
+            {
+                Console.WriteLine("El angulo debe ser menor o igual a 45°!");
+            }
         }
     }
 }
