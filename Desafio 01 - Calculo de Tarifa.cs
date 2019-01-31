a0a5d08b8a32427b0fcbf2308c9e2a7be7c915e9
Christian Andrade <46961345+ChistianA@users.noreply.github.com>
2019-01-31 18:54:47 -0500

    Add files via upload

---
+++ Desafio 01 - Calculo de Tarifa.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Desafio01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Selector de Tarifa
+
+            Console.WriteLine("Ingrese su SMMLV para calcular su tarifa >> ");
+            double dinero = double.Parse(Console.ReadLine());
+
+            double smmlv = dinero / 828.116; 
+
+            //Condiciones
+
+            if (smmlv < 2)
+            {
+                Console.WriteLine("> Tarifa A <");
+            }
+            else if (smmlv >= 2 & smmlv < 4)
+            {
+                Console.WriteLine("> Tarifa B <");
+            }
+            else
+            {
+                Console.WriteLine("> Tarifa C <");
+            }
+        }
+    }
+}
