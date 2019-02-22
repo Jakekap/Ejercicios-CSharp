b7f822a086bfe518a3b5b331ed0446c11e0952ba
Christian Andrade <46961345+ChistianA@users.noreply.github.com>
2019-02-22 06:39:05 -0500

    Update Juego del Azar.cs

--- Juego del Azar.cs
+++ Juego del Azar.cs
@@ -14,56 +14,41 @@ namespace Juego_del_Azar
             int dado1 = 0, dado2 = 0, total = 0, contador = 0, tiradas=0, dobles=0;
             double media = 0;
             string continuar = "y";
-
-
-            while (continuar == "y" && (dado1 + dado2 != 2) )
-            {
+            while (continuar == "y" && (dado1 + dado2 != 2) ){
                 dado1 = aleatorio.Next(1, 7);
                 dado2 = aleatorio.Next(1, 7);
                 Console.WriteLine("Dados :" + dado1 + " : " + dado2);
                 tiradas += 1;
-                if (dado1 == 1 && dado2 == 1)
-                {
+                if (dado1 == 1 && dado2 == 1){
                     total = 0;
-                    Console.WriteLine("Eliminado!");
-                    
+                    Console.WriteLine("Eliminado!");  
                 }
-                else
-                {
+                else{
                     total += dado1 + dado2;
                     Console.WriteLine("Total: " + total);
                     Console.WriteLine("Desea continiar? (y/n): ");
-                    continuar = Console.ReadLine();
-                    
+                    continuar = Console.ReadLine();  
                 }
-                if (dado1 + dado2 >= 6)
-                {
-                    contador += 1;
-                    
+                if (dado1 + dado2 >= 6){
+                    contador += 1; 
                 }
-                if (dado1 == dado2)
-                {
+                if (dado1 == dado2){
                     dobles += 1;
-                    if (dobles == 3)
-                    {
+                    if (dobles == 3){
                         continuar = "n";
                         Console.WriteLine("Ganaste, sacaste 3 pares seguidos!!!");
                     }
                 }
                 else dobles = 0;
                 
-                if(total >= 100)
-                {
+                if(total >= 100){
                     Console.WriteLine("Ganaste!");
                     continuar = "n";
-                    
                 }
-               
-            }
+               }
             media = ((double)(contador) / tiradas) * 100;
             Console.WriteLine("Su total fue: " + total + " puntos");
             Console.WriteLine("Su porcentaje de tiradas mayores a 6 es del: " + Math.Round(media, 2) + "%");
-
         }
     }
 }
