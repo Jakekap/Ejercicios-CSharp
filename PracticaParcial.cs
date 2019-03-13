fb3b267805d5eef45a39c2e25b9a3a4b963d3c19
Christian Andrade <46961345+ChistianA@users.noreply.github.com>
2019-03-13 07:59:55 -0500

    Add files via upload

---
+++ PracticaParcial.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using QA;
+
+class Program {
+    static void Main() {
+        Evaluation evaluation = new Evaluation();
+    }
+}
+
